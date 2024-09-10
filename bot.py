5818009e96a8f89ed5fb29cdcf70af369722f300
The Wizard <149065372+SneetSnart420@users.noreply.github.com>
2024-09-10 08:02:27 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,80 +1,112 @@
-from datetime import datetime 
 import time
-import pomice 
+from datetime import datetime 
 
+import discord
 from discord.ext import commands
-import discord 
-from typing import Optional, List
 
+from colorama import init, Fore, Style
+
+init(autoreset=True)
+
+# Default mentions
+allowed_mentions = discord.AllowedMentions(roles=False, everyone=False, users=True) 
+
+# Intents, for more info: https://discordpy.readthedocs.io/en/latest/intents.html
+intents = discord.Intents(
+    voice_states=True,
+    messages=True,
+    reactions=True,
+    message_content=True,
+    guilds=True,
+    members=True
+)
 
 class DJ(commands.Bot):
-    def __init__(self, version_str: str, cogs: List[str],  prefix: str = ";", description: str = "Kittens that keep you jamming!") -> None:
-        # Allowed Mentions 
-        allowed_mentions = discord.AllowedMentions(roles=False, everyone=False, users=True)
-        
-        # Intents, for more info: https://discordpy.readthedocs.io/en/latest/intents.html
-        intents = discord.Intents(
-            guilds=True,
-            members=True,
-            bans=True,
-            emojis=True,
-            voice_states=True,
-            messages=True,
-            reactions=True,
-            message_content=True,
-        )
-        
-        # Initialize Bot
+    def __init__(self, config: dict) -> None:
+        self._prefix = config.get('prefix', '!!')
         super().__init__(
-            command_prefix=commands.when_mentioned_or(prefix),
-            intents=intents,
-            description=description,
-            allowed_mentions=allowed_mentions)
+            command_prefix=commands.when_mentioned_or(self._prefix), # Defaults to '!!'
+            description=config.get('description', "Somebody didn't change their description!"),
+            intents = intents,
+            allowed_mentions = allowed_mentions
+        )
+
+        self.config = config
         self.boot_start = time.time()
-        self._cogs = cogs 
+        
+        self._cogs = config.get('cogs', [])
         self._uptime = datetime.now()
-        self._version = version_str
-    
-    
+
+        self._version = config.get('version', '1.0.0') # version_str
+        
+        self._activity = config.get('activity', 'Somebody didn\'t change their activity!')
+        
+        self.log_colors = {
+                "INFO": Fore.CYAN,
+                "ERROR": Fore.RED,
+                "WARNING": Fore.YELLOW,
+                "SUCCESS": Fore.GREEN,  
+            }
+        self.total_songs = 0
     @property
     def owner(self) -> discord.User:
         return self.bot_app_info.owner
-        
+
+    def log(self, message: str, level: str = "INFO"):
+        level = level.upper() # Just in case.
+        color = self.log_colors.get(level, Fore.CYAN)
+        fill = '-'
+        print(f"{Fore.BLUE} {self.user}{Style.RESET_ALL} | {color}[{level:{fill}<7}]{Style.RESET_ALL}{Fore.WHITE} {message}{Style.RESET_ALL}")
+
     async def on_command_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
         if isinstance(error, commands.CommandNotFound):
+            self.log(f"Command not found: {ctx.message.content}", "WARNING")
             return
 
-        raise error
-    
+        self.log(f"Error in command {ctx.command}: {error}", "ERROR")
+        await ctx.send(f"An error occurred: {error}")
+
     async def on_ready(self) -> None:
-        print(
-            f"Username: {self.user} | {discord.__version__=}\nGuilds: {len(self.guilds)} | Users: {len(self.users)}|Prefix: {self.command_prefix}"
-        )
-        print(
-            f"Invite Link: https://discord.com/api/oauth2/authorize?client_id={self.user.id}&permissions=52288&scope=bot%20applications.commands"
-        )
-        print(f"Ready: {self.user} (ID: {self.user.id})")
+        """The reason I want to put this on github is so I can avoid ever writing this again.
+        If anybody knows a better, crossplatform method than just formatting ANSI, let me know."""
+        
+        self.log(f"Username: {self.log_colors["INFO"]}{self.user}{Style.RESET_ALL} | discord.py version: {self.log_colors["INFO"]}{discord.__version__}{Style.RESET_ALL}", "INFO")
+        self.log(f"Guilds: {self.log_colors["INFO"]}{len(self.guilds)}{Style.RESET_ALL} | Users: {self.log_colors["INFO"]}{len(self.users)}{Style.RESET_ALL} | Prefix: {self.log_colors["INFO"]}{self._prefix}{Style.RESET_ALL}", "INFO")
+        self.log(f"Invite Link: {self.log_colors["INFO"]}https://discord.com/api/oauth2/authorize?client_id={self.user.id}&permissions=52288&scope=bot%20applications.commands{Style.RESET_ALL}", "INFO")
+        self.log(f"Ready: {self.log_colors["SUCCESS"]}{self.user}{Style.RESET_ALL} (ID: {self.log_colors["SUCCESS"]}{self.user.id}{Style.RESET_ALL})", "SUCCESS")
     
     async def startup(self) -> None:
+        """
+        Cog syncing and boot time
+        """
         await self.wait_until_ready()
-        await self.cogs["Music"].start_nodes()
-        print("Music Nodes Started")
+        if self.cogs.get('Music'): # Music check
+            await self.cogs['Music'].start_nodes()
         await self.tree.sync()
-        print("Ready and Tree Synced")
-        done = time.time()
-        print(f"Booted in: {done - self.boot_start:.2f} seconds")
+        self.log("Ready and Tree Synced", "SUCCESS")
+        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=self._activity))
+        # Calculate boot time 
+        done = round(time.time() - self.boot_start, 2)
         
-    
+        # Assign Different levels to different colors
+        if done < 30:
+            self.log(f"Booted in: {self.log_colors["SUCCESS"]} {done} {Style.RESET_ALL}seconds", "SUCCESS")
+        elif done < 60:
+            self.log(f"Booted in: {self.log_colors["INFO"]} {done} {Style.RESET_ALL}seconds", "INFO")
+        elif done < 120:
+            self.log(f"Booted in: {self.log_colors["WARNING"]} {done} {Style.RESET_ALL}seconds", "WARNING")
+        else:
+            self.log(f"Booted in: {self.log_colors["ERROR"]} {done} {Style.RESET_ALL} seconds", "ERROR")
+        
     async def setup_hook(self) -> None:
         """Initialize cogs"""
-
         for cog in self._cogs:
             try:
-                print('Loading: %s' % cog)
+                self.log(f'Loading: {self.log_colors["INFO"]}{cog}{Style.RESET_ALL}', "INFO")
                 await self.load_extension(cog)
             except Exception as e:
-                print('Failed to load: %s - %s' % cog % e)
-            finally:
-                print('Loaded: %s' % cog)                
+                self.log(f"Failed to load cog {self.log_colors["ERROR"]}{cog}: {e}{Style.RESET_ALL}", "ERROR")
+            else:
+                self.log(f"Loaded: {self.log_colors["SUCCESS"]}{cog}{Style.RESET_ALL}", "SUCCESS")
         self.loop.create_task(self.startup())
         
--- main.py
+++ main.py
@@ -1,13 +1,35 @@
+
 from bot import DJ
-from discord.ext import commands
+from core.arghandler import make_parser, parse_logic
 
 cogs = [
-    'cogs.music',]
+    'cogs.music',
+    'cogs.admin',
+    'cogs.filters' # Filters is broken for now
+]
 
-version_str = '0.9'
+TOKEN = 'Get your own Token'
+version_str = '1.1.0'
 prefix = ';'
 description = 'Meow Bop MeowMeowMeow Bop'
-token = 'YOUR_TOKEN_HERE'
+
+##
+BOT_CONFIG = {
+    'token':TOKEN,
+    'id': '1',
+    'prefix':';',
+    'cogs':cogs,
+    'version':version_str,
+    'description': description,
+    'text_channel':'419065933844316162', # Music
+    'voice_channel':'419066880901251072', # Music VC
+    'activity':'use /play or ;play to play music!'
+}
+
 if __name__ == '__main__':
-    bot = DJ(version_str, cogs, prefix, description)
-    bot.run(token)
+    parser = make_parser()
+    config = parse_logic(parser, BOT_CONFIG)
+    if config == 0:
+        exit(0)
+    bot = DJ(config)
+    bot.run(config['token'])
